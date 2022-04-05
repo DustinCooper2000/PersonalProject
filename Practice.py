8804cc1c08cf9526379c67e50e847c09274c31d6
Dustin <Cooperd@berea.edu>
2022-04-04 22:18:21 -0400

    Added a new prompt for the user that will allow them to make multiple calculations with one run of the program

--- Practice.py
+++ Practice.py
@@ -11,25 +11,35 @@ class Calculator:
         self.operator = ""
         self.x = 0
         self.y = 0
+        self.again = ""
+        self.clear = ""
 
     def imply(self):
-        self.x = int(input("what is the first number you would like to use?"))
-        self.operator = input("Which operator would you like to use?")
-        self.y = int(input("What is the second number you would like to use?"))
-        if self.operator == "+":
-            self.add(self.x, self.y)
+        while self.again != "n":
+            self.x = int(input("what is the first number you would like to use?"))
+            self.operator = input("Which operator would you like to use?")
+            self.y = int(input("What is the second number you would like to use?"))
+            if self.operator == "+":
+                self.add(self.x, self.y)
 
-        elif self.operator == "-":
-            self.subtract(self.x, self.y)
+            elif self.operator == "-":
+                self.subtract(self.x, self.y)
 
-        elif self.operator == "*":
-            self.multiply(self.x, self.y)
+            elif self.operator == "*":
+                self.multiply(self.x, self.y)
 
-        elif self.operator == "/":
-            self.divide(self.x, self.y)
+            elif self.operator == "/":
+                self.divide(self.x, self.y)
 
-        elif self.operator == "**":
-            self.exponent(self.x, self.y)
+            elif self.operator == "**":
+                self.exponent(self.x, self.y)
+
+            self.again = str(input("Would you like to make another calculation? (y/n)")).lower()
+            print(self.again)
+            #if self.again == "y" or "Y":
+                #self.clear = input("Would you like to clear your result? (y/n)")
+                #if self.clear == "y" or "Y":
+                 #   self.
 
     def add(self, x, y):
         r = x + y
